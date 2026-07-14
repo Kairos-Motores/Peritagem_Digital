88c517b824ce2a59313145691551df2db54adf46
unknown <dodonou3@gmail.com>
2026-07-14 12:01:21 -0300

    Adicionando lógica de álbum fotográfico

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { SyncProvider } from './contexts/SyncContext';
 import { InspecaoProvider } from './contexts/InspecaoContext';
+import { OfflineProvider } from './contexts/OfflineContext'; // ← NOVO
 import ToastProvider from './components/ui/ToastProvider';
 import AppRoutes from './AppRoutes';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <AuthProvider>
         <SyncProvider>
           <InspecaoProvider>
-            <ToastProvider />
-            <AppRoutes />
+            <OfflineProvider>   {/* ← ENVOLVE A APLICAÇÃO */}
+              <ToastProvider />
+              <AppRoutes />
+            </OfflineProvider>
           </InspecaoProvider>
         </SyncProvider>
       </AuthProvider>
