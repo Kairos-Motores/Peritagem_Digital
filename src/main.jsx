33b27dc68db2033a752ad90ffafb0f708d925843
unknown <dodonou3@gmail.com>
2026-08-10 14:00:25 -0300

    Retirando backend

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from './contexts/ThemeContext';
+import 'virtual:pwa-register';
 import './styles/globals.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
